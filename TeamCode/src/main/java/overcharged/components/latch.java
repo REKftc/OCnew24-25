4c4f24c36b813f15e7c7f33f86d3f4bd3207fabb
sophiarwang <sophia.r.wang6@gmail.com>
2024-11-09 00:23:39 -0800

    latch

--- TeamCode/src/main/java/overcharged/components/latch.java
+++ TeamCode/src/main/java/overcharged/components/latch.java
@@ -6,10 +6,10 @@ public class latch {
     //public OcServo intakeTilt;
     public OcServo latch;
     //public VoltageSensor intakeVolt;
-    public static final float INIT = 206f;
+    public static final float INIT = 214f;//206f;
     // public static final float TRANSFER = 70f;//175f;
     // public static final float SPEC = 171f;//158f;
-    public static final float OUT = 237f;
+    public static final float OUT = 71f;//237f;
 
 
     public latch(HardwareMap hardwareMap) {
